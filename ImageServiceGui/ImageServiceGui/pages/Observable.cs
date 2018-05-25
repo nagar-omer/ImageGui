1c7d869488fb17b609144d78760904b115ce4340
ovednagar <33989713+ovednagar@users.noreply.github.com>
2018-05-25 15:21:14 +0300

    first upload
    view part - for gui

---
+++ ImageServiceGui/ImageServiceGui/pages/Observable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageServiceGui.pages {
+    // base class for observable classes (data for pages/windows)
+    public class Observable : INotifyPropertyChanged {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // general method for property changed event, the input is the name of the property
+        protected void OnPropertyChanged(string prop) {
+            if (PropertyChanged == null)
+                return;
+            PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ ImageServiceGui/ImageServiceGui/pages/SettingsPage.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ImageServiceGui.pages {
+    /// <summary>
+    /// Interaction logic for SettingsPage.xaml
+    /// </summary>
+     
+    // settings page User-Control 
+    public partial class SettingsPage : UserControl {
+
+        private SettingsData _settingsData;
+        public SettingsPage() {
+            // init observable object
+            ObservableCollection<string> y = new ObservableCollection<string>();
+            // init data
+            _settingsData = new SettingsData(y);
+            InitializeComponent();
+            DataContext = _settingsData;
+        }
+
+        // set of function to change properties of the page
+
+        public void SetOutDir(string outDir){
+            _settingsData.OutputDir = outDir;
+        }
+
+        public void SetSourceName(string source) {
+            _settingsData.SourceName = source;
+        }
+
+        public void SetLogName(string logger) {
+            _settingsData.LogName = logger;
+        }
+
+        public void SetThumbnailSize(int size) {
+            _settingsData.ThumbSize = size.ToString();
+        }
+
+        public void AddHndler(string handler) {
+            _settingsData.addHndler(handler);
+        }
+
+        public void RemoveHndler(string handler) {
+            _settingsData.removeHndler(handler);
+        }
+
+        public void EnableRemove() {
+            _settingsData.ClickEnabled = true;
+        }
+
+        public void DisableRemove() {
+            _settingsData.ClickEnabled = false;
+        }
+
+        private void OnRemoveButtonClicked(object sender, RoutedEventArgs e) {
+            // TODO activate view model func
+        }
+    }
+}
